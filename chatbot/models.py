2ec8ccdf61e4143b59ea2f39d7c9d39a5f9c05d2
Brin29 <breinerstivenparracortes50@gmail.com>
2025-02-20 15:36:50 -0500

    sa

--- chatbot/models.py
+++ chatbot/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Chat(models.Model):
-    message = models.TextField()
+    message = models.FileField()
     response = models.TextField()
