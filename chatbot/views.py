e476acfeb85bf743602f7fce5b6fca518a0c22e1
Brin29 <breinerstivenparracortes50@gmail.com>
2025-02-20 16:46:40 -0500

    inicio de la comunicacion con el pdf

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,26 +1,33 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from openai import OpenAI
 from .models import Chat
 from django.views.decorators.csrf import csrf_exempt
+import PyPDF3
 import json
 
-client = OpenAI(api_key="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", base_url="https://openrouter.ai/api/v1")
+client = OpenAI(api_key="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", base_url="https://openrouter.ai/api/v1")
 
 # Create your views here.
 def ask_openai(message):
-    response = client.chat.completions.create(
-    model="deepseek/deepseek-r1:free",
-    messages=[
-        {
-            "role": "user",
-            "content": message
-        }
-        ]
-    )
+    pdf_file = open("nomina.pdf", "rb")
+    pdf_reader = PyPDF3.PdfFileReader(pdf_file)
 
-    answer = response.choices[0].message.content
-    return answer
+    for page_num in range(pdf_reader.numPages):
+        page = pdf_reader.getPage(page_num)
+        text = page.extractText()
+
+        response = client.chat.completions.create(
+        model="deepseek/deepseek-r1:free",
+        messages=[
+            {
+                "role": "user",
+                "content": message + text
+            }
+            ]
+        )
+
+        answer = response.choices[0].message.content.strip()
+        return answer
     
 
 @csrf_exempt  # Esto es necesario si no estás utilizando CSRF tokens en tu API
